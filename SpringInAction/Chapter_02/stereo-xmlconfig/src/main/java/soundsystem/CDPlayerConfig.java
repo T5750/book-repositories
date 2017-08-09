5b4e6e6724ea27733285d081181be97c17e938e8
T5750 <evangel_a@sina.com>
2017-08-09 17:04:07 +0800

    added stereo-autoconfig, stereo-javaconfig, stereo-xmlconfig in Chapter_02.

---
+++ SpringInAction/Chapter_02/stereo-xmlconfig/src/main/java/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,17 @@
+package soundsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CDPlayerConfig {
+	@Bean
+	public CompactDisc compactDisc() {
+		return new SgtPeppers();
+	}
+
+	@Bean
+	public CDPlayer cdPlayer() {
+		return new CDPlayer(compactDisc());
+	}
+}
